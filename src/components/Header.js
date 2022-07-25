640a84054ab9aeffb7c037a1369cdbc25776f7d1
hnhtran <tran_eetas@yahoo.com>
2022-07-26 01:08:17 +1000

    Header complete

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+export default function Header() {
+    return(
+        <div className="text-center">
+            <img 
+            src="https://i.imgur.com/3KXm2jS.jpeg"
+            width="100"
+            className="rounded-circle"
+            alt="logo"
+            style={{marginTop: "20px"}}
+            />
+            <hr/>
+            <h5>
+                <i>presents</i>
+            </h5>
+            <h1>Trasia Watches Store - A place no other!</h1>
+        </div>
+    )
+}
