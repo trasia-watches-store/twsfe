bd09b64ef13deb2e049ba72a818a483b36b05361
hnhtran <tran_eetas@yahoo.com>
2022-07-26 03:44:39 +1000

    complete App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    
+    <Fragment>
+      <Header />
+      <Home />
+    </Fragment>
   );
 }
 
