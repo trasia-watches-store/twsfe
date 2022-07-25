6255595cb9b8bf3288bcf002de6e617918ab249a
hnhtran <tran_eetas@yahoo.com>
2022-07-26 03:41:39 +1000

    complete Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,36 @@
 import { Col, Container, Row } from 'reactstrap';
 import WatchList from './WatchList';
 import NewWatchModal from './NewWatchModal';
+import axios from axios
+import { API_URL } from '../constants/index';
+
+export default Home = (props) => {
+    const [watches, setWatches] = useState([]);
+
+    function componentDidMount() {
+        resetWatch();
+    }
+
+    function resetWatch() {
+        getWatches({});
+    }
+
+    function getWatches() {
+        axios.get(API_URL).then(res => setWatches({watches: res.data}));
+    }
+
+    return(
+        <Container style={{ marginTop: "20px" }}>
+            <Row>
+                <Col>
+                    <WatchList watches={watches} resetWatch={resetWatch}/>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <NewWatchModal create={true} resetWatch={resetWatch}/>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
