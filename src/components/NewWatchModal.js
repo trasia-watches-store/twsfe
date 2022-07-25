a47750ba05988d7f02f66041dd03036668c8e6fa
hnhtran <tran_eetas@yahoo.com>
2022-07-26 01:43:52 +1000

    complete NewWatchModal

--- src/components/NewWatchModal.js
+++ src/components/NewWatchModal.js
@@ -1,3 +1,36 @@
 import { Fragment } from 'react';
 import { Modal, Button, ModalHeader, ModalBody } from 'reactstrap';
 import NewWatchForm from './NewWatchForm';
+
+export default NewWatchModal = (props) => {
+    const [modal, setModal] = useState(false);
+
+    function toggle() {
+        setModal(!modal);
+    }
+
+    const create = props.create
+    let title = "Edit Watch"
+    let button = <Button onClick={toggle}>Edit</Button>
+
+    if (create) {
+        title = "Create New Watch"
+        button = <Button color="primary" className="float-right" onClick={toggle} style={{ minWidth: "200px" }}>Create</Button>
+    }
+
+    return (
+        <Fragment>
+            {button}
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>{title}</ModalHeader>
+                <ModalBody>
+                    <NewWatchForm 
+                    resetWatch={props.resetWatch}
+                    toggle={toggle}
+                    watch={props.watch}
+                    />
+                </ModalBody>
+            </Modal>
+        </Fragment>
+    );
+}
