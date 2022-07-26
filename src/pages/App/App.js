32b76e9e7b32df6edd06ede7f030f709fc89036b
hnhtran <tran_eetas@yahoo.com>
2022-07-27 08:12:39 +1000

    reorganise files

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from '../../components/Header';
+import Header from '../../components/Header/Header';
 import Home from '../HomePage/Home';
 // import NavBar from './components/NavBar/NavBar.js';
 import { Link } from 'react-router-dom';
