e87189352287ad704a88af06df896006d7c04777
hnhtran <tran_eetas@yahoo.com>
2022-07-27 07:53:34 +1000

    imports Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 import { Link } from 'react-router-dom';
 import { Fragment, useState } from 'react';
 // import { Navbar, Collapse, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarToggler } from 'reactstrap';
