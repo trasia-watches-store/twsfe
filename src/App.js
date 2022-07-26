fe5c760d96c1d81fddcffcf442871463aa70199c
hnhtran <tran_eetas@yahoo.com>
2022-07-27 05:41:02 +1000

    reactstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
-import Navbar from './components/Navbar';
-import { Fragment } from 'react';
+// import Navbar from './components/Navbar';
+import { Fragment, useState } from 'react';
+import { Navbar, Collapse, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
   return (
     <Fragment>
-      <Navbar/>
+      <Navbar>
+
+      </Navbar>
       <Header />
       <Home />
     </Fragment>
