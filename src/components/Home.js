6742d1c206d473e752d960ca3a69a2b00f698eb2
hnhtran <tran_eetas@yahoo.com>
2022-07-27 05:14:19 +1000

    back to class based

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,81 +7,81 @@ import axios from "axios"
 
 import { API_URL } from "../constants"
 
-// class Home extends Component {
-//     state = {
-//         watches: []
-//     }
-
-//     componentDidMount() {
-//         this.resetState()
-//     }
-
-//     getWatches = () => {
-//         axios.get(API_URL).then(res => this.setState({ watches: res.data }))
-//     }
-
-//     resetState = () => {
-//         this.getWatches()
-//     }
-
-//     render() {
-//         return (
-//             <Container style={{  marginTop: "20px" }}>
-//                 <Row>
-//                     <Col>
-//                         <WatchList 
-//                         watches={this.state.watches} 
-//                         resetState={this.resetState}
-//                         />
-//                     </Col>
-//                 </Row>
-//                 <Row>
-//                     <Col>
-//                         <NewWatchModal 
-//                         resetState={this.resetState} 
-//                         create={true}
-//                         />
-//                     </Col>
-//                 </Row>
-//             </Container>
-//         )
-//     }
-// }
-function Home() {
-    const [watches, setWatches] = useState([])
-
-    function componentDidMount() {
-        resetState()
+class Home extends Component {
+    state = {
+        watches: []
     }
 
-    const getWatches = () => {
-        axios.get(API_URL).then(res => setWatches({ watches: res.data }))
+    componentDidMount() {
+        this.resetState()
     }
 
-    const resetState = () => {
-        getWatches()
+    getWatches = () => {
+        axios.get(API_URL).then(res => this.setState({ watches: res.data }))
     }
 
-    return (
-        <Container style={{  marginTop: "20px" }}>
-            <Row>
-                <Col>
-                    <WatchList 
-                    watches={watches} 
-                    resetState={resetState}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <NewWatchModal 
-                    resetState={resetState} 
-                    create={true}
-                    />
-                </Col>
-            </Row>
-        </Container>
+    resetState = () => {
+        this.getWatches()
+    }
+
+    render() {
+        return (
+            <Container style={{  marginTop: "20px" }}>
+                <Row>
+                    <Col>
+                        <WatchList 
+                        watches={this.state.watches} 
+                        resetState={this.resetState}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <NewWatchModal 
+                        resetState={this.resetState} 
+                        create={true}
+                        />
+                    </Col>
+                </Row>
+            </Container>
         )
+    }
 }
+// function Home() {
+//     const [watches, setWatches] = useState([])
+
+//     function componentDidMount() {
+//         resetState()
+//     }
+
+//     const getWatches = () => {
+//         axios.get(API_URL).then(res => setWatches({ watches: res.data }))
+//     }
+
+//     const resetState = () => {
+//         getWatches()
+//     }
+
+//     return (
+//         <Container style={{  marginTop: "20px" }}>
+//             <Row>
+//                 <Col>
+//                     <WatchList 
+//                     watches={watches} 
+//                     resetState={resetState}
+//                     />
+//                 </Col>
+//             </Row>
+//             <Row>
+//                 <Col>
+//                     <NewWatchModal 
+//                     resetState={resetState} 
+//                     create={true}
+//                     />
+//                 </Col>
+//             </Row>
+//         </Container>
+//         )
+// }
 
 export default Home
