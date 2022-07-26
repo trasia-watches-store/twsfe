caed3c2b4c5512fd59e052d3e550d491605ce52a
hnhtran <tran_eetas@yahoo.com>
2022-07-27 07:53:00 +1000

    navbar in app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
-// import Navbar from './components/Navbar';
+import { Link } from 'react-router-dom';
 import { Fragment, useState } from 'react';
-import { Navbar, Collapse, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+// import { Navbar, Collapse, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarToggler } from 'reactstrap';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false)
   const toggle = () => setIsOpen(!isOpen)
   return (
     <Fragment>
-      <Navbar>
-
-      </Navbar>
+      <Navbar />
       <Header />
       <Home />
     </Fragment>
