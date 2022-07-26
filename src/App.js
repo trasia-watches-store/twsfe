53cdfd742fd2c1a09a1c60ad124e2377500a3425
hnhtran <tran_eetas@yahoo.com>
2022-07-27 05:18:08 +1000

    navbar test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 import { Fragment } from 'react';
 
 function App() {
   return (
     <Fragment>
+      <Navbar/>
       <Header />
       <Home />
     </Fragment>
