a220a64e682a835de40b42be0c6d9122bdbd7a0c
hnhtran <tran_eetas@yahoo.com>
2022-07-27 09:58:27 +1000

    imports staffprofile for app

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from '../../components/Header/Header';
 import HomeStaff from '../HomePage/HomeStaff';
+import StaffProfile from '../StaffProfile/StaffProfile';
 import NavBar from '../../components/NavBar/NavBar';
 import { Link } from 'react-router-dom';
 import { Fragment, useState } from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
+import StaffProfile from '../StaffProfile/StaffProfile';
 // import { Navbar, Collapse, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarToggler } from 'reactstrap';
 
 function App() {
@@ -21,6 +23,10 @@ function App() {
             <Route
               path="/managedb"
               element={<HomeStaff user={user} />}
+            ></Route>
+            <Route
+              path="/staffprofile"
+              element={<StaffProfile user={user} />}
             ></Route>
             {/* redirect to /orders/new if path in address bar hasn't matched a <Route> above */}
             {/* <Route path="/*" element={<Navigate to="/HomeStaff" />} /> */}
