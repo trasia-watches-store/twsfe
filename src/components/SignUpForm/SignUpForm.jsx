df0fca89f8b306d9a4a2c3c7fa69a4cc47f74b8e
hnhtran <tran_eetas@yahoo.com>
2022-07-27 14:28:24 +1000

    signupform connect data back and front complere

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import './SignUpForm.css';
+import React, { Component } from "react";
+import "./SignUpForm.css";
 import axios from "axios";
-import { API_URL } from "../../constants";
+import { USER_URL } from "../../constants";
 
 export default class SignUpForm extends Component {
   // class field syntax
-  
+
   state = {
-    username: '',
-    email: '',
-    password: '',
-    confirm: '',
-    error: ''
+    username: "",
+    email: "",
+    password1: "",
+    password2: "",
+    error: "",
   };
 
   // axios.post(API_URL, this.state).then(() => {
@@ -21,44 +21,74 @@ export default class SignUpForm extends Component {
 
   handleSubmit = async (evt) => {
     evt.preventDefault();
-    try {
-      const formData = {...this.state};
-      delete formData.error;
-      delete formData.confirm;
+    // try {
+    //   const formData = { ...this.state };
+    //   delete formData.error;
+    //   delete formData.confirm;
+      axios.post(`${USER_URL}registration/`, this.state).then(() => {
+        this.props.setUser();
+        // this.props.toggle();
+      });
       // const user = await signUp(formData);
       // console.log(user)
       // this.props.setUser(user);
-    } catch {
-      
-      this.setState({error: 'Sign Up Failed - Try Again'});
-    }
+    // } catch {
+    //   this.setState({ error: "Sign Up Failed - Try Again" });
+    // }
   };
 
   handleChange = (evt) => {
-    
     this.setState({
       [evt.target.name]: evt.target.value,
-      error: ''
+      error: "",
     });
   };
 
-  
   render() {
     const disable = this.state.password !== this.state.confirm;
     return (
       <div>
         <div className="form-container">
-            <h1>Trasia Watches Store</h1>
+          <h1>Trasia Watches Store</h1>
           <form autoComplete="off" onSubmit={this.handleSubmit}>
-            
-            <input type="text"placeholder="Username" name="name" value={this.state.name} onChange={this.handleChange} required />
-        
-            <input type="email"placeholder='Email' name="email" value={this.state.email} onChange={this.handleChange} required />
-            
-            <input type="password" placeholder='Password' name="password" value={this.state.password} onChange={this.handleChange} required />
-            
-            <input type="password" placeholder='Confirm' name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
-            <button type="submit" disabled={disable}>SIGN UP</button>
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+              required
+            />
+
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              required
+            />
+
+            <input
+              type="password"
+              placeholder="Password"
+              name="password1"
+              value={this.state.password1}
+              onChange={this.handleChange}
+              required
+            />
+
+            <input
+              type="password"
+              placeholder="Confirm"
+              name="password2"
+              value={this.state.password2}
+              onChange={this.handleChange}
+              required
+            />
+            <button type="submit" disabled={disable}>
+              SIGN UP
+            </button>
           </form>
         </div>
         <p className="error-message">&nbsp;{this.state.error}</p>
