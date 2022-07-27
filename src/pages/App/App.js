17937e465f0465b8de23d43794a522e9366e2e4a
hnhtran <tran_eetas@yahoo.com>
2022-07-27 10:22:41 +1000

    authpage and app linked

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Header from '../../components/Header/Header';
 import HomeStaff from '../HomePage/HomeStaff';
 import NavBar from '../../components/NavBar/NavBar';
+import AuthPage from '../AuthPage/AuthPage';
+import StaffProfile from '../StaffProfile/StaffProfile';
 import { Link } from 'react-router-dom';
 import { Fragment, useState } from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
-import StaffProfile from '../StaffProfile/StaffProfile';
 // import { Navbar, Collapse, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarToggler } from 'reactstrap';
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
 
   return (
     <Fragment>
+    {user ? (
+      <>
       <NavBar user={user} setUser={setUser} />
       {/* <Header /> */}
       {/* <Home /> */}
@@ -32,6 +35,13 @@ function App() {
             {/* redirect to /orders/new if path in address bar hasn't matched a <Route> above */}
             {/* <Route path="/*" element={<Navigate to="/HomeStaff" />} /> */}
           </Routes>
+          </>
+          ) : (
+            <Routes>
+              <Route path="/" element={<AuthPage setUser={setUser} />} />
+            </Routes>
+            // <AuthPage setUser={setUser} />
+          )}
     </Fragment>
   );
 }
