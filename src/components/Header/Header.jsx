bac8b62476cdd96f289a85523c0b39be36c33bc1
hnhtran <tran_eetas@yahoo.com>
2022-08-01 00:59:36 +1000

    header styled

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,9 @@
+import './Header.css'
+
 export default function Header() {
     return(
         <div className="text-center">
-            {/* <img 
-            src="https://www.static.vecteezy.com/photo/1428286-analog-fashion-watch-on-white-background.jpeg"
-            width="300"
-            className="rounded-circle"
-            alt="logo"
-            style={{marginTop: "20px"}}
-            />
-            <hr/> */}
-            {/* <h5>
-                <i>presents</i>
-            </h5> */}
-            <h1 style={{marginTop: '20px'}}>Trasia Watches Store - A place no other!</h1>
+            <h1>Trasia Watches Store - A place no other!</h1>
         </div>
     )
 }
