4ef4924a375bc5ff2217a943db100744b259c85d
hnhtran <tran_eetas@yahoo.com>
2022-08-01 03:18:13 +1000

    footer styled

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 export default function Footer(){
     return(
         <footer>
-            <h4>Created by:</h4>
+            <h4 className='text-footer'>Created by:</h4>
             <div className='img-wrapper'>
             <a href="https://github.com/hnhtran"><img src="/images/tranGH.jpeg" className='creator' alt="githublogo" /></a></div> 
         </footer>
