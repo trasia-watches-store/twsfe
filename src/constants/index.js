94747614c385e69cd0d0c4ba55e71c2cb89f69d4
hnhtran <tran_eetas@yahoo.com>
2022-08-02 04:32:04 +1000

    deploy prepare

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,4 @@
-export const API_URL="http://localhost:8000/api/watches/";
-export const USER_URL="http://localhost:8000/api/users/dj-rest-auth/";
+// export const API_URL="http://localhost:8000/api/watches/";
+// export const USER_URL="http://localhost:8000/api/users/dj-rest-auth/";
+export const API_URL="https://trasia-api.herokuapp.com/api/watches/";
+export const USER_URL="https://trasia-api.herokuapp.com/api/users/dj-rest-auth/";
--- src/constants/test.js
+++
@@ -1,6 +0,0 @@
-// export const API_URL="http://localhost:8000/api/watches/";
-// export const USER_URL="http://localhost:8000/api/users/dj-rest-auth/";
-// export const API_URL="http://localhost:8000/api/watches/";
-console.log(process.env.REACT_APP_API_URL);
-console.log(process.env);
-// export const USER_URL="http://localhost:8000/api/users/dj-rest-auth/";
