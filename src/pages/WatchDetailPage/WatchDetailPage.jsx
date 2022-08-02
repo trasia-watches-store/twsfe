9a278dc000d06ac62516ccb4f9ce2c4fb76b9699
hnhtran <tran_eetas@yahoo.com>
2022-08-02 21:09:43 +1000

    import useParams

--- src/pages/WatchDetailPage/WatchDetailPage.jsx
+++ src/pages/WatchDetailPage/WatchDetailPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './WatchDetailPage.css'
 import { ListGroup, ListGroupItem } from 'reactstrap'
+import { useParams } from 'react-router-dom'
 
 const WatchDetailPage = ({user, watches}) => {
     let watch = watches.find(watch => watch.pk == 19)
