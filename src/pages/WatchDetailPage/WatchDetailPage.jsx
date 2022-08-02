e40feced5f91557b95deacb4c47a9415990a9ffe
hnhtran <tran_eetas@yahoo.com>
2022-08-02 21:08:48 +1000

    import reactstrap components

--- src/pages/WatchDetailPage/WatchDetailPage.jsx
+++ src/pages/WatchDetailPage/WatchDetailPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './WatchDetailPage.css'
+import { ListGroup, ListGroupItem } from 'reactstrap'
 
 const WatchDetailPage = ({user, watches}) => {
     let watch = watches.find(watch => watch.pk == 19)
